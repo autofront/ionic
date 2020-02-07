a523ada65ad0c8067a3913c072f35ea9c225fbae
velrino <denismagalhaesvelrino@gmail.com>
2020-02-07 03:04:01 -0300

    :zap: added firebase module and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjVJaGMjWiKTlI2x69XcLGAw_yooJBerk",
+  authDomain: "autofront-velrino.firebaseapp.com",
+  databaseURL: "https://autofront-velrino.firebaseio.com",
+  projectId: "autofront-velrino",
+  storageBucket: "autofront-velrino.appspot.com",
+  messagingSenderId: "1075287772936",
+  appId: "1:1075287772936:web:df3944ad443027f000e6bd"
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
